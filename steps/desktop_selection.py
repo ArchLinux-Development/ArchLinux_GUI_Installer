af3924cace243c12db397f972465af3c92a326fb
Eliminater74 <eliminater74@gmail.com>
2024-06-25 19:45:37 -0400

    updated Desktops

--- steps/desktop_selection.py
+++ steps/desktop_selection.py
@@ -5,6 +5,17 @@ class DesktopSelection(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
+        self.software_options = {
+            "KDE Plasma": ["Konsole", "Dolphin", "Kate", "Gwenview", "Okular", "Spectacle"],
+            "GNOME": ["GNOME Terminal", "Nautilus", "Gedit", "Evince", "GNOME Screenshot"],
+            "XFCE": ["XFCE Terminal", "Thunar", "Mousepad", "Ristretto", "XFCE Screenshooter"],
+            "Cinnamon": ["GNOME Terminal", "Nemo", "Gedit", "Evince", "GNOME Screenshot"],
+            "MATE": ["MATE Terminal", "Caja", "Pluma", "Atril", "Mate Screenshot"],
+            "LXQt": ["QTerminal", "PCManFM-Qt", "FeatherPad", "LXImage-Qt", "LXQt Archiver"],
+            "Budgie": ["Tilix", "Nautilus", "Gedit", "Evince", "GNOME Screenshot"],
+            "i3": ["i3", "i3status", "i3lock", "dmenu"],
+            "Deepin": ["Deepin Terminal", "Deepin File Manager", "Deepin Text Editor", "Deepin Image Viewer"]
+        }
         self.create_widgets()
 
     def create_widgets(self):
@@ -30,7 +41,7 @@ class DesktopSelection(ttk.Frame):
         label = ttk.Label(self, text="Select Desktop Environment:")
         label.pack(fill='x', expand=True)
 
-        self.desktop_env = ttk.Combobox(self, values=["GNOME", "KDE Plasma", "XFCE", "Cinnamon", "MATE", "LXQt", "Budgie", "i3", "Deepin"])
+        self.desktop_env = ttk.Combobox(self, values=list(self.software_options.keys()))
         self.desktop_env.pack(fill='x', expand=True)
         self.desktop_env.current(0)
 
@@ -40,5 +51,6 @@ class DesktopSelection(ttk.Frame):
     def next_step(self):
         selected_desktop = self.desktop_env.get()
         print(f"Selected Desktop Environment: {selected_desktop}")
+        self.parent.selected_desktop = selected_desktop
         from steps.software_selection import SoftwareSelection
         self.parent.show_step(SoftwareSelection)
--- steps/software_selection.py
+++ steps/software_selection.py
@@ -5,15 +5,27 @@ class SoftwareSelection(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
+        self.software_options = {
+            "KDE Plasma": ["Konsole", "Dolphin", "Kate", "Gwenview", "Okular", "Spectacle"],
+            "GNOME": ["GNOME Terminal", "Nautilus", "Gedit", "Evince", "GNOME Screenshot"],
+            "XFCE": ["XFCE Terminal", "Thunar", "Mousepad", "Ristretto", "XFCE Screenshooter"],
+            "Cinnamon": ["GNOME Terminal", "Nemo", "Gedit", "Evince", "GNOME Screenshot"],
+            "MATE": ["MATE Terminal", "Caja", "Pluma", "Atril", "Mate Screenshot"],
+            "LXQt": ["QTerminal", "PCManFM-Qt", "FeatherPad", "LXImage-Qt", "LXQt Archiver"],
+            "Budgie": ["Tilix", "Nautilus", "Gedit", "Evince", "GNOME Screenshot"],
+            "i3": ["i3", "i3status", "i3lock", "dmenu"],
+            "Deepin": ["Deepin Terminal", "Deepin File Manager", "Deepin Text Editor", "Deepin Image Viewer"]
+        }
         self.create_widgets()
 
     def create_widgets(self):
-        label = ttk.Label(self, text="Select Software Options:")
+        selected_desktop = self.parent.selected_desktop
+        label = ttk.Label(self, text=f"Select Software Options for {selected_desktop}:")
         label.pack(fill='x', expand=True)
 
-        self.software_options = ["Minimal", "Normal", "Maximal"]
-        self.software_var = tk.StringVar(value=self.software_options)
-        self.software_listbox = tk.Listbox(self, listvariable=self.software_var, selectmode='single')
+        self.software_list = self.software_options.get(selected_desktop, [])
+        self.software_var = tk.StringVar(value=self.software_list)
+        self.software_listbox = tk.Listbox(self, listvariable=self.software_var, selectmode='multiple')
         self.software_listbox.pack(fill='both', expand=True)
 
         button = ttk.Button(self, text="Next", command=self.next_step)
@@ -21,10 +33,8 @@ class SoftwareSelection(ttk.Frame):
 
     def next_step(self):
         selected_software_indices = self.software_listbox.curselection()
-        if not selected_software_indices:
-            print("No software option selected")
-            return
-        selected_software = self.software_options[selected_software_indices[0]]
+        selected_software = [self.software_list[i] for i in selected_software_indices]
         print(f"Selected Software: {selected_software}")
+        self.parent.selected_software = selected_software
         from steps.filesystem_selection import FilesystemSelection
         self.parent.show_step(FilesystemSelection)
